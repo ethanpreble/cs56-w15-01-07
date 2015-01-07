96fcca43ab2befcb5c3e1c1e6b554ec3c3a88486
ethanpreble <ethanpreble@umail.ucsb.edu>
2015-01-07 10:41:01 -0800

    added the Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/**
+Hello World program
+@author ethanpreble
+@version cs56
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+    }
+}
